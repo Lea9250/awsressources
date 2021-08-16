929af4ebdbd387f000690794db97f2376bde8a66
Lea Droguet <ljdroguet@gmail.com>
2021-08-16 09:47:26 +0000

    filter search in aws query directly

--- agent/Unix/Awsressources.pm
+++ agent/Unix/Awsressources.pm
@@ -52,83 +52,69 @@ sub awsressources_inventory_handler {
                 );
     
     foreach my $region (@regions) {
+        # Other query candidate that might be faster ... might be
+        # aws ec2 --profile ocs describe-instances --query "Reservations[].Instances[].{InstanceId: InstanceId, ImageId: ImageId, Type: InstanceType, Key: KeyName, LaunchTime: LaunchTime, Monitoring: Monitoring.State, AvailabilityZone: Placement.AvailabilityZone, PrivateDns: PrivateDnsName, PrivateIP: PrivateIpAddress, Status: State.Name, StatusReason: StateTransitionReason, SubnetId: SubnetId, VpcId: VpcId, Architecture: Architecture, Networks: NetworkInterfaces,RootDeviceName: RootDeviceName, RootDeviceType: RootDeviceType, CpuCores: CpuOptions.CoreCount, CpuThreadsPerCore: CpuOptions.ThreadsPerCore}" --region us-east-2
+        
         # Instances will be retrieved for every region specified above
-        my $result = `aws ec2 --profile $profileName describe-instances --region $region`;
+        my $result = `aws ec2 --profile $profileName describe-instances --query "Reservations[].Instances[].{InstanceId: InstanceId, ImageId: ImageId, Type: InstanceType, Key: KeyName, LaunchTime: LaunchTime, Monitoring: Monitoring.State, AvailabilityZone: Placement.AvailabilityZone, PrivateDns: PrivateDnsName, PrivateIP: PrivateIpAddress, Status: State.Name, StatusReason: StateTransitionReason, SubnetId: SubnetId, VpcId: VpcId, Architecture: Architecture, Networks: NetworkInterfaces,RootDeviceName: RootDeviceName, RootDeviceType: RootDeviceType, CpuCores: CpuOptions.CoreCount, CpuThreadsPerCore: CpuOptions.ThreadsPerCore}" --region $region`;
         $result = decode_json $result;
 
-        if (@{$result->{Reservations}}) {
-            $logger->debug("Generation xml data for region : $region");        
-            # reservations level
-            foreach my $reservation (@{$result->{Reservations}}) {
-                my $reservationId = $reservation->{ReservationId};
-                my $ownerId = $reservation->{OwnerId};
-                
-                # instance level
-                foreach my $instance (@{$reservation->{Instances}}) {
-                    # Add XML
-                    push @{$common->{xmltags}->{AWS_INSTANCES}},
+        if (@{$result}) {
+            $logger->debug("Generating xml data for region : $region");         
+            print Dumper($result);             
+            # instance level
+            foreach my $instance (@{$result}) {
+                push @{$common->{xmltags}->{AWS_INSTANCES}},
+                {
+                    RESERVATION_ID => "whatever",
+                    OWNER_ID => "still whatever",
+                    INSTANCE_ID => [$instance->{InstanceId}],
+                    INSTANCE_TYPE => [$instance->{Type}],
+                    LAUNCH_TIME => [$instance->{LaunchTime}],
+                    AVAILABILTY_ZONE => [$instance->{AvailabilityZone}],
+                    ARCHITECTURE => [$instance->{Architecture}],
+                    KEY_NAME => [$instance->{Key}],
+                    IMAGE_ID => [$instance->{ImageId}],
+                    # AMI_LAUNCH_INDEX => [$instance->{AmiLaunchIndex}],
+                    MONITORING => [$instance->{Monitoring}],
+                    # STATE_CODE => [$instance->{State}{Code}],
+                    STATE_NAME => [$instance->{Status}],
+                    STATE_REASON => [$instance->{StatusReason}],
+                    # VIRTUALIZATION_TYPE => [$instance->{VirtualizationType}],
+                    # HIBERNATION_OPT_CONFIGURED => [JSON::PP::is_bool($instance->{HibernationOptions}->{Configured})],
+                    # ENCLAVED_OPT_ENABLED => [JSON::PP::is_bool($instance->{EnclaveOptions}->{Enabled})],
+
+                };
+
+                push @{$common->{xmltags}->{AWS_INSTANCES_HARDWARE}},
                     {
-                        RESERVATION_ID => [$reservationId],
-                        OWNER_ID => [$ownerId],
+                        RESERVATION_ID => "whatever",
+                        OWNER_ID => "still whatever",
                         INSTANCE_ID => [$instance->{InstanceId}],
-                        INSTANCE_TYPE => [$instance->{InstanceType}],
-                        LAUNCH_TIME => [$instance->{LaunchTime}],
-                        AVAILABILTY_ZONE => [$instance->{Placement}{AvailabilityZone}],
-                        ARCHITECTURE => [$instance->{Architecture}],
-                        KEY_NAME => [$instance->{KeyName}],
-                        IMAGE_ID => [$instance->{ImageId}],
-                        AMI_LAUNCH_INDEX => [$instance->{AmiLaunchIndex}],
-                        MONITORING => [$instance->{Monitoring}{State}],
-                        STATE_CODE => [$instance->{State}{Code}],
-                        STATE_NAME => [$instance->{State}{Name}],
-                        STATE_REASON => [$instance->{StateTransitionReason}],
-                        VIRTUALIZATION_TYPE => [$instance->{VirtualizationType}],
-                        HIBERNATION_OPT_CONFIGURED => [JSON::PP::is_bool($instance->{HibernationOptions}->{Configured})],
-                        ENCLAVED_OPT_ENABLED => [JSON::PP::is_bool($instance->{EnclaveOptions}->{Enabled})],
+                        ROOT_DEVICE_NAME => [$instance->{RootDeviceName}],
+                        ROOT_DEVICE_TYPE => [$instance->{RootDeviceType}],
+                        CPU_CORE_COUNT => [$instance->{CpuCores}],
+                        CPU_THREADS_PER_CORE => [$instance->{CpuThreadsPerCore}],
+                    };
+
+                foreach my $networkscat (@{$instance->{Networks}}) {
+                    push @{$common->{xmltags}->{AWS_INSTANCES_NETWORKS}},
+                    {
+                        RESERVATION_ID => "whatever",
+                        OWNER_ID => "still whatever",
+                        INSTANCE_ID => [$instance->{InstanceId}],
+                        MAC_ADDR => [$networkscat->{MacAddress}],
+                        PRIVATE_DNS_NAME => [$instance->{PrivateDns}],
+                        PRIVATE_IP_ADDR => [$instance->{PrivateIP}],
+                        #PUBLIC_DNS_NAME => [$networkscat->{PublicDnsName}],
+                        VPC_ID => [$instance->{VpcId}],
+                        NETWORK_INTERFACE_ID => [$networkscat->{NetworkInterfaceId}],
+                        SUBNET_ID => [$instance->{SubnetId}],
 
                     };
 
-                    foreach my $networkscat (@{$instance->{NetworkInterfaces}}) {
-                        push @{$common->{xmltags}->{AWS_INSTANCES_NETWORKS}},
-                        {
-                            RESERVATION_ID => [$reservationId],
-                            OWNER_ID => [$ownerId],
-                            INSTANCE_ID => [$instance->{InstanceId}],
-                            MAC_ADDR => [$networkscat->{MacAddress}],
-                            PRIVATE_DNS_NAME => [$networkscat->{PrivateDnsName}],
-                            PRIVATE_IP_ADDR => [$networkscat->{PrivateIpAddress}],
-                            PUBLIC_DNS_NAME => [$networkscat->{PublicDnsName}],
-                            VPC_ID => [$networkscat->{VpcId}],
-                            NETWORK_INTERFACE_ID => [$networkscat->{NetworkInterfaceId}],
-                            SUBNET_ID => [$networkscat->{SubnetId}],
-
-                        };
-
-                    }
-
-
-                    foreach my $blockcat (@{$instance->{BlockDeviceMappings}}) {
-                        push @{$common->{xmltags}->{AWS_INSTANCES_HARDWARE}},
-                        {
-                            RESERVATION_ID => [$reservationId],
-                            OWNER_ID => [$ownerId],
-                            INSTANCE_ID => [$instance->{InstanceId}],
-                            ROOT_DEVICE_NAME => [$instance->{RootDeviceName}],
-                            ROOT_DEVICE_TYPE => [$instance->{RootDeviceType}],
-                            CPU_CORE_COUNT => [$instance->{CpuOptions}{CoreCount}],
-                            CPU_THREADS_PER_CORE => [$instance->{CpuOptions}{ThreadsPerCore}],
-                            HYPERVISOR => [$instance->{Hypervisor}],
-                            BLOCK_DEVICE_NAME => [$blockcat->{DeviceName}],
-                            # BLOCK_DEVICE_TYPE => $blockcat->{Ebs},
-                            BLOCK_DEVICE_ATTACH_TIME => [$blockcat->{Ebs}{AttachTime}],
-                            BLOCK_DEVICE_DELETE_ON_TERMINATION => [JSON::PP::is_bool($blockcat->{Ebs}{DeleteOnTermination})],
-                            BLOCK_DEVICE_STATUS => [$blockcat->{Ebs}{Status}],
-                            BLOCK_DEVICE_VOLUME_ID => [$blockcat->{Ebs}{VolumeId}],
-                        };
-                        
-                    }
-
                 }
+                
             }
         } else {
             $logger->debug("No instances found for region : $region");
